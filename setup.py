dff8bb28c2268d229b104f8b8759c40fe6818361
Jenkins Automation Server <jenkins@IGSCHSi-0ee068.usgs.chs.ead>
2017-11-01 15:29:23 +0000

    Bump version: 0.2.0dev → 0.2.0

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ def read(filepath):
 requirements = read_requirements('requirements.txt')
 
 setup(name='usgs_wma_mlr_ddot_ingester',
-      version='0.2.0dev',
+      version='0.2.0',
       description='MLR Ddot Ingester Microservice',
       author='Mary Bucknell, Andrew Yan, Dave Steinich, Zack Moore, Kathy Schoephoester',
       author_email='mlr-devs@usgs.gov',
