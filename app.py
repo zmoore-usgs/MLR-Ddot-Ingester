1ac53fe654af1e099e9e7fc72c7e816cf6c5bb25
Mary Bucknell <mbucknell@usgs.gov>
2017-11-09 09:06:23 -0600

    Added code to add authentication to the service endpoint. Add requirements for flask_restplus_jwt, cryptography, and requests

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import os
+import requests
+
 
 from flask import Flask
 
@@ -9,6 +11,11 @@ application.config.from_object('config')
 PROJECT_DIR = PROJECT_DIR = os.path.dirname(__file__)
 if os.path.exists(os.path.join(PROJECT_DIR, '.env')):
     application.config.from_pyfile('.env')
+
+if application.config.get('AUTH_TOKEN_KEY_URL'):
+    resp = requests.get(application.config.get('AUTH_TOKEN_KEY_URL'), verify=application.config['VERIFY_CERT'])
+    application.config['JWT_PUBLIC_KEY'] = resp.json()['value']
+    application.config['JWT_ALGORITHM'] = 'RS256'
 
 from services import *
 
--- config.py
+++ config.py
@@ -1,1 +1,19 @@
+import os
+
+
 DEBUG = False
+
+AUTH_TOKEN_KEY_URL = os.getenv('AUTH_TOKEN_KEY_URL', '')
+
+JWT_DECODE_AUDIENCE = 'trusted-app'
+
+
+def get_role(dict):
+    return dict.get('authorities')
+
+
+JWT_ROLE_CLAIM = get_role
+
+VERIFY_CERT = True
+
+
